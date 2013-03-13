2ca77e7f78b25296e0b9f5141a144bd138928fb6
Simeon Filipov <ssfilipov@gmail.com>
2013-03-13 10:19:49 +0000

    Remove unnecessary tests from ValveTests. Close #8

--- src/test/test/components/ValveTests.java
+++ src/test/test/components/ValveTests.java
@@ -1,56 +1,13 @@
 package components;
 
-import static org.junit.Assert.assertEquals;
-
-import model.FlowType;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import components.Valve;
-
-
-
+/**
+ * The test class is deliberately empty:
+ * Valve doesn't contain a single method with
+ * cyclomatic complexity greater than 1.
+ * 
+ * @author Simeon
+ *
+ */
 public class ValveTests {
-
-	private Valve valve;
-	private int valveID = 4;
-	private FlowType valveFlowType = FlowType.Water;
-	private boolean valveOpen = true;
-
-	@Before
-	public void setUp() {
-		this.valve = new Valve(this.valveID, this.valveFlowType, this.valveOpen);
-	}
-	
-	/*
-	 * Asserts the getID method returns the same value that the Pump was instantiated with(this.valveId).
-	 */
-	@Test
-	public void testGetIDReturnsTheCorrectValue() {
-		
-		assertEquals( this.valveID, valve.getID());
-		
-	}
-	
-	/*
-	 * Asserts that when the valveOpen variable is set to true, the isOpen method returns the same value
-	 */
-	@Test
-	public void testValveCanBeSetToOpen() {		
-		
-		valve.setOpen(true);		
-		assertEquals(true, valve.isOpen());		
-	}
-	
-	/*
-	 * Asserts that when the valveOpen variable is set to false, the isOpen method returns the same value
-	 */
-	@Test
-	public void testValveCanBeSetToClosed() {		
-		
-		valve.setOpen(false);		
-		assertEquals(false, valve.isOpen());	
-	}	
 
 }
